b0e2abf9d01f8b6f99149c57397bbdb6a142d067
Prasoon Gupta <praveengupta1969.pg@gmail.com>
2025-06-22 04:43:22 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
-export const serverUrl = "http://localhost:3000";
+export const serverUrl = "https://unichat-app-backend.onrender.com";
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
