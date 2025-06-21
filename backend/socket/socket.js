0031ab46e023177efa699915fcf1a9050c83924b
Prasoon Gupta <praveengupta1969.pg@gmail.com>
2025-06-22 04:56:59 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ let app=express();
 export const server=http.createServer(app);
 const io=new Server(server,{
     cors:{
-        origin: "http://localhost:5173",
+        origin: "https://unichat-app-n7nx.onrender.com",
     }
 })
 const userSocketMap={};
