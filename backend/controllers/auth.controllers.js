5066d81a526eba555190afd2235e627467d1d6f5
Prasoon Gupta <praveengupta1969.pg@gmail.com>
2025-06-22 04:59:03 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -29,6 +29,8 @@ try{
     res.cookie("token",token,{
         httpOnly:true,
         maxAge: 7*24*60*60*1000,
+        sameSite:"None",
+        secure:true
     })
     return res.status(201).json(user);
 
@@ -59,6 +61,8 @@ try{
     res.cookie("token",token,{
         httpOnly:true,
         maxAge: 7*24*60*60*1000,
+        sameSite:"None",
+        secure:true
     });
 
     return res.status(200).json(isUserExist);
